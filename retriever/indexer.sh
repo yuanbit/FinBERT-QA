e3b777f6fd94c604ab4863a4fb171c10d5e81390
Bithiah Yuan <bithiahy@gmail.com>
2020-04-14 15:13:28 +0200

    changed indexer.sh

--- retriever/indexer.sh
+++ retriever/indexer.sh
@@ -1,5 +1,5 @@
 # Index documents using Lucene document indexer
 
 sh anserini/target/appassembler/bin/IndexCollection -collection JsonCollection \
- -generator LuceneDocumentGenerator -threads 9 -input retrieval/collection_json \
- -index retrieval/lucene-index-fiqa -storePositions -storeDocvectors -storeRawDocs
+ -generator LuceneDocumentGenerator -threads 9 -input collection_json \
+ -index lucene-index-fiqa -storePositions -storeDocvectors -storeRawDocs
